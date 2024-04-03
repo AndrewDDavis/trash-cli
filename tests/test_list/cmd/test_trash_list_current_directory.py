03d9b6fa899a4607ee2bf0edc90810f1cdcf5e21
Andrea Francia <260852+andreafrancia@users.noreply.github.com>
2024-04-03 22:24:58 +0200

    Fix: using the new TrashListUser API and fix a typo

--- tests/test_list/cmd/test_trash_list_current_directory.py
+++ tests/test_list/cmd/test_trash_list_current_directory.py
@@ -12,37 +12,38 @@ class TestTrashListCurrentDirectory(Setup):
     def setUp(self):
         super(type(self), self).setUp()
         self.user.set_fake_cwd("/home/user/currentdir")
-                                   
+
     def test_currentdir_files_only(self):
-        self.user.home_trashdir.add_trashinfo2('/home/user/currentdir/file1',
-                                               datetime(2001, 2, 3, 23, 55, 59))
-        self.user.home_trashdir.add_trashinfo2('/home/user/otherdir/file2',
-                                               datetime(2001, 2, 3, 23, 55, 59))
+        self.user.add_home_trashinfo('/home/user/currentdir/file1',
+                                     "2001-02-03 23:55:59")
+        self.user.add_home_trashinfo('/home/user/otherdir/file2',
+                                     "2001-02-03 23:55:59")
 
-        self.user.run_trash_list('--currentdir')
+        output = self.user.run_trash_list('--currentdir')
 
-        assert_equals_with_unidiff("2001-02-03 23:55:59 " + "/home/user/currentdir/file1\n",
-                                   self.user.output())
+        assert_equals_with_unidiff(
+            "2001-02-03 23:55:59 /home/user/currentdir/file1\n",
+            output.whole_output())
 
     def test_currentdir_nested_folders(self):
-        self.user.home_trashdir.add_trashinfo2('/home/user/otherdir/currentdir/file1',
-                                               datetime(2001, 2, 3, 23, 55, 59))
-        self.user.home_trashdir.add_trashinfo2('/home/user/currentdir/newdirectory/file2',
-                                               datetime(2001, 2, 3, 23, 55, 59))
+        self.user.add_home_trashinfo('/home/user/otherdir/currentdir/file1',
+                                     "2001-02-03 23:55:59")
+        self.user.add_home_trashinfo('/home/user/currentdir/newdirectory/file2',
+                                     "2001-02-03 23:55:59")
 
-        self.user.run_trash_list('--currentdir')
+        output = self.user.run_trash_list('--currentdir')
 
-        assert_equals_with_unidiff("2001-02-03 23:55:59 " + "/home/user/currentdir/newdirectory/file2\n",
-                                   self.user.output())
+        assert_equals_with_unidiff(
+            "2001-02-03 23:55:59 /home/user/currentdir/newdirectory/file2\n",
+            output.whole_output())
 
+    def test_should_output_currentdir_should_not_show_currentdir_itself(self):
+        self.user.add_home_trashinfo("/home/user/currentdir",
+                                     "2001-02-03 23:55:59")
+        self.user.add_home_trashinfo('/home/user/otherdir/file1',
+                                     "2001-02-03 23:55:59")
 
-    def test_should_output_currendir_should_not_show_currentdir_itself(self):
-        self.user.home_trashdir.add_trashinfo2("/home/user/currentdir",
-                                               datetime(2001, 2, 3, 23, 55, 59))
-        self.user.home_trashdir.add_trashinfo2('/home/user/otherdir/file1',
-                                               datetime(2001, 2, 3, 23, 55, 59))
-
-        self.user.run_trash_list('--currentdir')
+        output = self.user.run_trash_list('--currentdir')
 
         assert_equals_with_unidiff("",
-                                   self.user.output())
+                                   output.whole_output())
